d2aa182ba3229c175de0dbfc76a2ccffc4c8d4a6
Gabe Cook <gabe565@gmail.com>
2024-03-06 15:57:23 -0600

    fix(rand): Fix rand param not being used

--- internal/util/rand.go
+++ internal/util/rand.go
@@ -3,12 +3,10 @@ package util
 import (
 	"crypto/rand"
 	"math/big"
-
-	"github.com/gabe565/pwgen-go/internal/wordlists"
 )
 
 func CryptoRandn(n int) (int, error) {
-	val, err := rand.Int(rand.Reader, big.NewInt(int64(wordlists.EnLines)))
+	val, err := rand.Int(rand.Reader, big.NewInt(int64(n)))
 	if err != nil {
 		return 0, err
 	}
