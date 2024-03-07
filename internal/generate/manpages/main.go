4b46301cbeeb61586bf5f82798fbe31b58c17314
Gabe Cook <gabe565@gmail.com>
2024-03-06 18:33:47 -0600

    fix(man): Fix sub man pages not being generated

--- internal/generate/manpages/main.go
+++ internal/generate/manpages/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"compress/gzip"
+	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
@@ -25,11 +27,6 @@ func main() {
 		panic(err)
 	}
 
-	date, err := time.Parse(time.RFC3339, dateParam)
-	if err != nil {
-		panic(err)
-	}
-
 	if err := os.RemoveAll("manpages"); err != nil {
 		panic(err)
 	}
@@ -39,34 +36,61 @@ func main() {
 	}
 
 	rootCmd := cmd.New()
-	name := rootCmd.Name()
+	rootName := rootCmd.Name()
 
-	header := doc.GenManHeader{
-		Title:   strings.ToUpper(name),
-		Section: "1",
-		Date:    &date,
-		Source:  name + " " + version,
-		Manual:  "User Commands",
-	}
-
-	f, err := os.Create(filepath.Join("manpages", name+".1.gz"))
+	date, err := time.Parse(time.RFC3339, dateParam)
 	if err != nil {
 		panic(err)
 	}
-	gz := gzip.NewWriter(f)
 
-	if err := doc.GenMan(rootCmd, &header, gz); err != nil {
+	header := doc.GenManHeader{
+		Title:   strings.ToUpper(rootName),
+		Section: "1",
+		Date:    &date,
+		Source:  rootName + " " + version,
+		Manual:  "User Commands",
+	}
+
+	if err := doc.GenManTree(rootCmd, &header, "manpages"); err != nil {
 		panic(err)
 	}
 
-	if err := gz.Close(); err != nil {
-		panic(err)
-	}
-	if err := f.Close(); err != nil {
-		panic(err)
-	}
+	if err := filepath.Walk("manpages", func(path string, info fs.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return err
+		}
 
-	if err := os.Chtimes(f.Name(), date, date); err != nil {
+		in, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		out, err := os.Create(path + ".gz")
+		if err != nil {
+			return err
+		}
+		gz := gzip.NewWriter(out)
+
+		if _, err := io.Copy(gz, in); err != nil {
+			return err
+		}
+
+		if err := in.Close(); err != nil {
+			return err
+		}
+		if err := os.Remove(path); err != nil {
+			return err
+		}
+
+		if err := gz.Close(); err != nil {
+			return err
+		}
+		if err := out.Close(); err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
 		panic(err)
 	}
 }
