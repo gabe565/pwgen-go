aa282fdf147c0b96d683c7f4a94400d1477afc98
Gabe Cook <gabe565@gmail.com>
2024-03-10 22:40:14 -0500

    Revert "chore(util): Change from `string` to `[]byte` in template funcs"
    This reverts commit 9c4439fa3d59abbc1363575d45ad56123d4e380a.

--- internal/util/strings.go
+++ internal/util/strings.go
@@ -9,20 +9,20 @@ import (
 	"github.com/gabe565/pwgen-go/internal/wordlists"
 )
 
-func RandWord() ([]byte, error) {
+func RandWord() (string, error) {
 	var line int
 	for line < 32 {
 		var err error
 		if line, err = CryptoRandn(wordlists.EnLines); err != nil {
-			return nil, err
+			return "", err
 		}
 	}
 
 	return GetLine(bytes.NewReader(wordlists.En), line)
 }
 
-func RandWords(n int) ([][]byte, error) {
-	result := make([][]byte, 0, n)
+func RandWords(n int) ([]string, error) {
+	result := make([]string, 0, n)
 	for i := 0; i < n; i += 1 {
 		word, err := RandWord()
 		if err != nil {
@@ -35,15 +35,15 @@ func RandWords(n int) ([][]byte, error) {
 
 var ErrNotEnoughLines = errors.New("not enough lines")
 
-func GetLine(r io.Reader, line int) ([]byte, error) {
+func GetLine(r io.Reader, line int) (string, error) {
 	scanner := bufio.NewScanner(r)
 	for i := 0; scanner.Scan(); i += 1 {
 		if i == line {
-			return scanner.Bytes(), nil
+			return scanner.Text(), nil
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		return nil, err
+		return "", err
 	}
-	return nil, ErrNotEnoughLines
+	return "", ErrNotEnoughLines
 }
