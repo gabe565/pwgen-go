ebcd77b0f4b80fa6f7bb5cb86952373ece202c9c
Gabe Cook <gabe565@gmail.com>
2024-09-11 00:03:56 -0500

    docs: Include version flag in generated docs

--- cmd/opt.go
+++ cmd/opt.go
@@ -10,6 +10,7 @@ type Option func(cmd *cobra.Command)
 func WithVersion(version string) Option {
 	return func(cmd *cobra.Command) {
 		cmd.Version = buildVersion(version)
+		cmd.InitDefaultVersionFlag()
 	}
 }
 
